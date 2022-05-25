5b612260f1f85d163d500bfe39dd2abebcd4ecab
João Pedro A <jaaopbr@gmail.com>
2022-05-25 18:19:48 -0300

    intial commit

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,20 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+interface ProfileProps {
+    showProfileData: boolean;
+};
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+    return (
+        <Flex align="center">
+            {showProfileData && (
+                <Box mr="4" textAlign="right" >
+                    <Text>João Pedro Arruda</Text>
+                    <Text color="gray.300" fontSize="small" >jaaopbr@gmail.com</Text>
+                </Box>
+            )}
+
+            <Avatar size="md" name="João Pedro Arruda" src="https://github.com/7cass.png" />
+        </Flex>
+    );
+}
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,129 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import Pagination from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+import { useQuery } from "react-query";
+
+export default function UserList() {
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users');
+        const data = await response.json();
+
+        return data;
+    });
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    });
+
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+                        <Link href="/users/create" passHref>
+                            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
+                                Criar novo
+                            </Button>
+                        </Link>
+                    </Flex>
+
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                        <Th w="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">João Pedro Arruda</Text>
+                                                <Text fontSize="sm" color="gray.300">jaaopbr@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>04 de Maio, 2022</Td>}
+                                        <Td>
+                                            {isWideVersion && (
+                                                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                    Editar
+                                                </Button>
+                                            )}
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">João Pedro Arruda</Text>
+                                                <Text fontSize="sm" color="gray.300">jaaopbr@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>04 de Maio, 2022</Td>}
+                                        <Td>
+                                            {isWideVersion && (
+                                                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                    Editar
+                                                </Button>
+                                            )}
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">João Pedro Arruda</Text>
+                                                <Text fontSize="sm" color="gray.300">jaaopbr@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>04 de Maio, 2022</Td>}
+                                        <Td>
+                                            {isWideVersion && (
+                                                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                                    Editar
+                                                </Button>
+                                            )}
+                                        </Td>
+                                    </Tr>
+                                </Tbody>
+                            </Table>
+
+                            <Pagination />
+                        </>
+                    )}
+                </Box>
+            </Flex>
+        </Box>
+    );
+}
